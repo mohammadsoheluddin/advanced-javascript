784bec309c5fd023dd68569204b8accd1d6adfda
mohammadsoheluddin <62943893+mohammadsoheluddin@users.noreply.github.com>
2020-08-07 12:38:02 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,223 @@
+/**const age = 4;
+
+if(age > 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+*/
+
+/** 
+const age = -4;
+
+if(age){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+*/
+
+/** 
+const age = -4;
+
+if(age > 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+*/
+
+/** 
+const age = 0;
+
+if(age){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+*/
+
+/** 
+const name = "Solaiman Khan";
+
+if(name.length > 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+*/
+
+/** 
+const name = "Solaiman Khan";
+
+if(name.length){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+*/
+
+/** 
+const name = "Solaiman Khan";
+
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+*/
+
+/** 
+const name = "";
+
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+*/
+
+/** 
+const name = " ";
+
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+*/
+
+/** 
+const name = "0";
+
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+*/
+
+/**
+let name;
+console.log(name);
+// it will be undefined by Javascript
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+ */
+
+ 
+/** 
+let name = null;
+console.log(name);
+// it will be undefined by Javascript
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+*/
+
+/** 
+let name = NaN;
+console.log(name);
+// it will be undefined by Javascript
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+*/
+
+
+
+// Falsy:
+// 0
+// ""
+// undefined
+// null
+// NaN
+// Truthy:
+// '0', ' '
+
+
+/** 
+let name = [];
+// result will be true
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+*/
+
+/** 
+let name = {};
+// result will be true
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+*/
+
+
+/** 
+let name = false;
+// result will be false
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+*/
+
+/** 
+let name = 'false';
+// result will be true
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+*/
+
+
+/** 
+let name = '0';
+
+if(name || name == 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+*/
+
+let name = '12';
+
+if(name || name == 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
