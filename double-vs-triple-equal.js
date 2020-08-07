71873a7921a20b448a749f24f1aeb41bb82f8f66
mohammadsoheluddin <62943893+mohammadsoheluddin@users.noreply.github.com>
2020-08-07 20:42:17 +0600

    double-vs-triple-equal concept learning

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,112 @@
+/** 
+const first = 2;
+const second = 3;
+if(first == second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+*/
+
+
+/** 
+const first = 2;
+const second = 2;
+if(first == second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+*/
+
+
+/** 
+const first = 2;
+const second = "2";
+if(first == second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+*/
+
+
+
+/** 
+const first = 2;
+const second = "2";
+if(first === second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+*/
+
+
+/** 
+const first = 2;
+const second = true;
+if(first === second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+*/
+
+
+/** 
+const first = 1;
+const second = true;
+if(first == second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+*/
+
+
+/** 
+const first = 1;
+const second = false;
+if(first == second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+*/
+
+
+/** 
+const first = 0;
+const second = false;
+if(first == second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+*/
+
+
+/**
+const first = 0;
+const second = false;
+if(first === second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+ */
+
+
+//  == type converting 
+
+// ===  value & type checking/compare.
