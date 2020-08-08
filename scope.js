8fd78ff32af73650f51e7817c00919875e2d9071
mohammadsoheluddin <62943893+mohammadsoheluddin@users.noreply.github.com>
2020-08-08 16:10:09 +0600

    advanced javascript practice done

---
+++ scope.js
@@ -0,0 +1,235 @@
+/** 
+function sum(first, second){
+    let result = first + second;
+    return result;
+}
+
+const output = sum(3, 7);
+console.log(output);
+*/
+
+
+/** 
+function sum(first, second){
+    let result = first + second;
+    console.log(result);
+    return result;
+}
+
+const output = sum(3, 7);
+console.log(output);
+*/
+
+
+/** 
+let bonus = 20;
+function sum(first, second){
+    let result = first + second + bonus;
+    console.log(bonus);
+    return result;
+}
+
+const output = sum(3, 7);
+console.log(bonus);
+console.log(output);
+*/
+
+
+/** 
+let bonus = 20;
+function sum(first, second){
+    let result = first + second + bonus;
+    // console.log(bonus);
+    if(result > 9){
+        const mood = "happy";
+        console.log(mood);
+    }
+    return result;
+}
+
+const output = sum(3, 7);
+// console.log(bonus);
+// console.log(output);
+*/
+
+
+/** 
+let bonus = 20;
+function sum(first, second){
+    let result = first + second + bonus;
+    // console.log(bonus);
+    if(result > 9){
+        const mood = "happy";
+        console.log(mood);
+    }
+    // console.log(mood);
+    return result;
+}
+
+const output = sum(3, 7);
+// console.log(bonus);
+// console.log(output);
+*/
+
+
+/** 
+let bonus = 20;
+function sum(first, second){
+    let result = first + second + bonus;
+    // console.log(bonus);
+    if(result > 9){
+        let mood = "happy";
+        mood = "fishy";
+        mood = "funky";
+        mood = "cranky"
+        console.log(mood);
+    }
+    console.log(mood);
+    return result;
+}
+
+const output = sum(3, 7);
+// console.log(bonus);
+// console.log(output);
+*/
+
+
+
+/** 
+let bonus = 20;
+function sum(first, second){
+    let result = first + second + bonus;
+    // console.log(bonus);
+    if(result > 9){
+        var mood = "happy";
+        mood = "fishy";
+        mood = "funky";
+        mood = "cranky"
+        console.log(mood);
+    }
+    console.log(mood);
+    return result;
+}
+
+const output = sum(3, 7);
+// console.log(bonus);
+// console.log(output);
+*/
+
+/** 
+let bonus = 20;
+function sum(first, second){
+    let result = first + second + bonus;
+    // console.log(bonus);
+    if(result > 9){
+        var mood = "happy";
+        mood = "fishy";
+        mood = "funky";
+        mood = "cranky"
+        // console.log(mood);
+    }
+    console.log(mood);
+    return result;
+}
+
+const output = sum(3, 7);
+// console.log(bonus);
+// console.log(output);
+*/
+
+
+/** 
+let bonus = 20;
+function sum(first, second){
+    let result = first + second + bonus;
+    // console.log(bonus);
+    if(result > 9){
+        var mood = "happy";
+        mood = "fishy";
+        mood = "funky";
+        mood = "cranky"
+        // console.log(mood);
+    }
+    console.log(day);
+    var day = "friday";
+    return result;
+}
+
+const output = sum(3, 7);
+// console.log(bonus);
+// console.log(output);
+*/
+
+
+/** 
+let bonus = 20;
+function sum(first, second){
+    let result = first + second + bonus;
+    // console.log(bonus);
+    if(result > 9){
+        var mood = "happy";
+        mood = "fishy";
+        mood = "funky";
+        mood = "cranky"
+        // console.log(mood);
+    }
+    var day = "friday";
+    console.log(day);
+    return result;
+}
+
+const output = sum(3, 7);
+// console.log(bonus);
+// console.log(output);
+*/
+
+
+/** 
+let bonus = 20;
+function sum(first, second){
+    let result = first + second + bonus;
+    // console.log(bonus);
+    if(result > 9){
+        var mood = "happy";
+        mood = "fishy";
+        mood = "funky";
+        mood = "cranky"
+        // console.log(mood);
+    }
+    console.log(day);
+    let day = "friday";
+    return result;
+}
+
+const output = sum(3, 7);
+// console.log(bonus);
+// console.log(output);
+*/
+
+
+let bonus = 20;
+function sum(first, second){
+    let result = first + second + bonus;
+    // console.log(bonus);
+    if(result > 9){
+        var mood = "happy";
+        mood = "fishy";
+        mood = "funky";
+        mood = "cranky"
+        // console.log(mood);
+    }
+    let day = "friday";
+    console.log(day);
+    return result;
+}
+
+const output = sum(3, 7);
+// console.log(bonus);
+// console.log(output);
+
+
+
+
+// thatjsdude.com..................................
+
+
